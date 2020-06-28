b358b10c1cc03c9b32ca170486c57cfcecc69db6
Tim Miller <timmil@cisco.com>
2020-06-28 15:34:50 -0400

    CCS Agent runs as root but then runs node init scripts as cliqruser.  Update script to sudo all the commands.

--- ccs/setup_django.sh
+++ ccs/setup_django.sh
@@ -3,7 +3,7 @@
 # Assumptions:
 #  - App Package directory is /opt/remoteFiles/appPackage/BRKCLD-2002-CLUS20-4.0
 #    - GitHub repository name (github.com/gve-vse-tim/BRKCLD-2002-CLUS20)
-#  - This script is run with root privileges
+#  - This script is run as cliquser
 #  - This script is executed AFTER the database (clus20_database) is created
 
 CCSWM_AGENT=/usr/local/agentlite
@@ -19,26 +19,31 @@ source ${CCSWM_AGENT}/etc/userenv
 DB_HOST=${CliqrDependencies}
 IP_VAR=CliqrTier_${DB_HOST}_IP
 DB_IP=${!IP_VAR}
-echo "${DB_IP}   ${DB_HOST}" >> /etc/hosts
+sudo su -c "echo '${DB_IP}   ${DB_HOST}' >> /etc/hosts"
 
 # Software installation
-sudo apt-get install -y git iproute2 python3-pip virtualenv \
-    apache2 libapache2-mod-wsgi-py3
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y git rsync iproute2 \
+    python3-pip virtualenv apache2 libapache2-mod-wsgi-py3
 
-sudo apt-get install -y python-dev default-libmysqlclient-dev mysql-client
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python-dev default-libmysqlclient-dev mysql-client
 
 # Create Python Virtual Environment
 if ! /usr/bin/test -d ${WEB_ROOT}; then
-    /bin/mkdir -p ${WEB_ROOT}
+    sudo /bin/mkdir -p ${WEB_ROOT}
 fi
 
 # Create Python virtual environment
-/bin/cp ${PARENT_SRC}/requirements.txt ${WEB_ROOT}
-/bin/bash ${PARENT_SRC}/docker/create_python_venv.sh
+sudo /bin/cp ${PARENT_SRC}/requirements.txt ${WEB_ROOT}
+sudo /bin/bash ${PARENT_SRC}/docker/create_python_venv.sh
 
 # Initialize Django secret key
-source ${WEB_ROOT}/venv/bin/activate
-python3 ${PARENT_SRC}/docker/init_django.py
+sudo su -c "source ${WEB_ROOT}/venv/bin/activate && python3 ${PARENT_SRC}/docker/init_django.py"
+
+# Copy website code to final destination
+sudo /bin/cp -r ${PARENT_SRC}/clus20_django ${WEB_ROOT}
+
+# Copy connection info to proper destination
+sudo /bin/cp ${PARENT_SRC}/docker/mysql-connection.json ${WEB_ROOT}/connection.json
 
 # Copy Django3 Apache configuration file
 if ! /usr/bin/test -d ${HTTPD_CONFD}; then
